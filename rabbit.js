3b79b5b5b0cbca0b5063a66981ac1ef351e6a117
MRB <mehrabshafae@gmail.com>
2024-12-11 17:28:36 +0330

    mrb: cu

--- rabbit.js
+++ rabbit.js
@@ -1,26 +1,62 @@
-import amqp from 'amqplib';
 import { alertCaller } from './alert.js';
-export const connectToRabbitMQ = async (queue) => {
+import { Core } from "@marboris/coreutils";
+
+class RabbitMQManager extends Core {
+  constructor() {
+    super();
+    this.channel = null;
+  }
+
+  async init() {
+    if (!this.config.EnvConfig.amqp) {
+      throw new Error("[env] amqp is required.");
+    }
     try {
-        const connection = await amqp.connect($.env.config.amqp);
-        const channel = await connection.createChannel();
-        await channel.assertQueue(queue, { durable: true });
-        process.once('SIGINT', async () => {
-            await channel.close();
-            await connection.close();
-        });
-        await channel.prefetch(1);
-        await channel.consume(queue, (message) => {
-            if (message) {
-                const msgContent = JSON.parse(message.content.toString());
-                console.log(" [x] Received '%s' from queue '%s'", msgContent, queue);
-                void alertCaller(msgContent);
-                channel.ack(message);
-            }
-        }, { noAck: false });
-        console.log(` [*] Waiting for messages in queue '${queue}'. To exit press CTRL+C`);
+      await this.amqpManager.connect(this.config.EnvConfig.amqp);
+      this.channel = this.amqpManager.getChannel();
+      console.log("RabbitMQ connection and channel created.");
+    } catch (err) {
+      console.error("Error initializing RabbitMQ:", err);
+      throw err;
     }
-    catch (err) {
-        console.warn(err);
+  }
+
+  async rec(queue) {
+    if (!this.channel || !queue) {
+      throw new Error("Channel is not initialized.");
     }
-};
+
+    await this.channel.assertQueue(queue, { durable: true });
+    await this.channel.prefetch(1);
+    await this.channel.consume(
+      queue,
+      (message) => {
+        if (message) {
+          const msgContent = JSON.parse(message.content.toString());
+          console.log(" [x] Received '%s' from queue '%s'", msgContent, queue);
+          void alertCaller(msgContent);
+          this.channel.ack(message);
+        }
+      },
+      { noAck: false }
+    );
+    console.log(
+      ` [*] Waiting for messages in queue '${queue}'. To exit press CTRL+C`
+    );
+  }
+
+  async close() {
+    await this.amqpManager.close();
+    console.log("RabbitMQ connection closed.");
+  }
+}
+
+(async () => {
+  const rabbitMQManager = new RabbitMQManager();
+  try {
+    await rabbitMQManager.init();
+    await rabbitMQManager.rec(rabbitMQManager.config.Args.queue);
+  } catch (err) {
+    console.error("Error occurred during message sending:", err);
+  }
+})();
