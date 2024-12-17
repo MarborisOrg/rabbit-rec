5af17caf5ff40250a1450e7f59c2849331305cd6
MRB <mehrabshafae@gmail.com>
2024-12-17 18:09:26 +0330

    mrb: cu

--- alert.js
+++ alert.js
@@ -1,22 +1,28 @@
 import { ApiBot } from "./alert/rabbit.alert.telegram.apibot.service.js";
 import { MidBot } from "./alert/rabbit.alert.telegram.mtp.service.js";
 
-let apibot
-let midbot
+let apibot;
+let midbot;
 
-export async function alertStarter(EnvConfig){
-    midbot = new MidBot()
-    apibot = new ApiBot()
+export async function alertStarter(EnvConfig) {
+  midbot = new MidBot();
+  apibot = new ApiBot();
 
-    await midbot.start(EnvConfig)
-    apibot.start(EnvConfig)
+  await midbot.start(EnvConfig);
+  apibot.start(EnvConfig);
 }
 
 export function alertCaller(msgContent) {
-    if (msgContent && typeof msgContent.item_id === 'string') {
-        console.log(msgContent.item_id);
+  if (msgContent && typeof msgContent.status === "string") {
+    if(msgContent.status === 'apibot'){
+        console.log(msgContent.status);
+        console.log(msgContent.msg);
+        console.log(msgContent.id);
+    
+        apibot.sendMessageToUser(msgContent.id, msgContent.msg);
     }
-    else {
-        console.log('item_id is not defined or not a string');
-    }
+    
+  } else {
+    console.log("status is not defined or not a string");
+  }
 }
